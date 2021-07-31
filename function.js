3414b0208dc64fe05b6f404fc4f475562866e528
Najmul H. Bappy <http.bappy@gmail.com>
2021-07-31 06:41:04 +0600

    Selection sort algorithm visualizer

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 var data = [23, 15, 16, 8, 5, 4, 19, 22];
 var svg, bandScale;
+createChart();
 
 function createChart() {
   svg = d3.select("#chart").append("svg");
 
   var h = 100,
-    w = 500;
+    w = 800;
 
   //var bandWidth = w / data.length - 1;
 
@@ -36,23 +37,18 @@ function createChart() {
     })
     .style("fill", "steelblue");
 }
-createChart();
-
-function change() {
-  /* var sortComparer;
-  sortComparer = function (a, b) {
-    return b - a;
-  };
-
-  data.sort(sortComparer);
+document.getElementById("random-data").addEventListener("click", function () {
+  data = [];
+  for (var i = 0; i < 20; i++) {
+    data.push(Math.floor(Math.random() * 100) + 1);
+  }
   console.log(data);
-  var dOrder = data.map(function (d) {
-    return d;
-  });
-  */
-
+  svg.remove();
+  createChart();
+});
+function selectionSort() {
   const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-  async function selectionSort() {
+  async function sort() {
     // We need to wrap the loop into an async function for this to work
     for (var i = 0; i < data.length; i++) {
       smallest = data[i];
@@ -63,32 +59,34 @@ function change() {
           pos = j;
         }
       }
-      temp = data[i];
-      data[i] = smallest;
-      data[pos] = temp;
-      var dOrder = data.map(function (d) {
-        return d;
-      });
-      bandScale.domain(dOrder);
-      svg
-        .transition()
-        .duration(750)
-        .selectAll("rect")
-        .attr("x", function (d) {
-          return bandScale(d);
+      if (data[i] != smallest) {
+        temp = data[i];
+        data[i] = smallest;
+        data[pos] = temp;
+        var dOrder = data.map(function (d) {
+          return d;
         });
+        bandScale.domain(dOrder);
+        svg
+          .transition()
+          .duration(750)
+          .selectAll("rect")
+          .attr("x", function (d) {
+            return bandScale(d);
+          });
+        var swooshAudio = new Audio("/sound-effects/swoosh.mp3");
+        swooshAudio.play();
+      }
+
       await timer(1000); // then the created Promise can be awaited
     }
-    svg.selectAll("rect").style("fill", "blue");
-    alert("Sorted");
+    svg.selectAll("rect").style("fill", "green");
+    var completeAudio = new Audio("/sound-effects/complete.mp3");
+    completeAudio.play();
   }
-  selectionSort();
+  sort();
 }
 
-d3.select("button").on("change", change);
-
-document.getElementById("selection-sort").addEventListener("click", change);
-
-var theRandomNumber = Math.floor(Math.random() * 10) + 1;
-
-console.log(theRandomNumber);
+document
+  .getElementById("selection-sort")
+  .addEventListener("click", selectionSort);
