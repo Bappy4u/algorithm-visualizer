206a92c7903a2e18354bbc8f30a037ea334e9f25
Bappy <http.bappy@gmail.com>
2023-06-18 15:23:18 +0600

    AV-2: Initialized vue component structure for algorithm visualizer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Algorithm-visualizer msg="Welcome to Algorithm Visualizer"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlgorithmVisualizer from './components/AlgorithmVisualizer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlgorithmVisualizer
   }
 }
 </script>
---
+++ src/routes/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import SearchView from './../algorithms/search/Search.vue';
+import homeView from './../algorithms/algorithms.vue';
+import SortView from './../algorithms/sort/Sort.vue';
+
+const routes = [
+    { path: '/', component: homeView },
+    { path: '/search', component: SearchView },
+    { path: '/sort', component: SortView },
+    // Add more routes for other algorithm components
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
