00bfd32c00e9e3ea792d1bca145950d4884f01f4
Bappy <http.bappy@gmail.com>
2023-07-20 00:56:47 +0600

    AV-07: Added linked to the menu and folder structured of linked list

---
+++ src/algorithms/linked-list/Linked-List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Linked List</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Linked-List"
+    }
+</script>
+
+<style scoped>
+
+</style>
